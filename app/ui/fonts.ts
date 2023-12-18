1cc6928cad7a0583d5a0b559b3e251af5328eede
Jose Abadia <jr.avalug@gmail.com>
2023-12-18 15:49:31 -0500

    last commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,18 @@
+import { Inter, Lusitana, Montserrat, Oswald } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const montserrat = Montserrat({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const oswald = Oswald({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
